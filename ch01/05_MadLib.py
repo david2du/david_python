6b064011ffc4a23389f2530bf8617515b43c772b
david2du <david2du@163.com>
2020-06-20 06:41:56 +0800

    init

--- ch01/05_MadLib.py
+++ ch01/05_MadLib.py
@@ -1,5 +1,6 @@
 #05MadLib.py
 adjective = input("Please input an adjective\n")
-noun = input("Please input a noun\n")
+noun1 = input("Please input a noun\n")
+noun2 = input("Please input another noun\n")
 verb = input("Please input a verb\n")
-print("The", adjective, noun, verb, "over David.")
+print("The", adjective, noun1, verb, "over", noun2)
